31614ed4b5ba475dddefe140288eacd4af679fba
oleksandr-ishchuk <alexmway@mail.ru>
2023-05-13 20:00:15 +0200

    add comments for class

--- src/clas/NumerHost.php
+++ src/clas/NumerHost.php
@@ -1,6 +1,17 @@
 <?php
 namespace src\clas;
 
+/**
+ * class returns the number of hosts in the network, subtracting the number of bits from 32,
+ * dedicated to the network address
+ * session variable is used as an input parameter
+ * $_SESSION['ipMask']
+ * класс возвращает число хостов в сети, отнимая от 32-х число битов, 
+ * выделенных под адрес сети
+ * в качестве входного параметра используется переменная сессии 
+ * $_SESSION['ipMask']
+ */
+
 class NumerHost
 {
     static private $link = null;
