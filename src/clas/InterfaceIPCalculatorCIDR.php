b0d983238bda2b1679cff5d96bc40072d392044e
oleksandr-ishchuk <alexmway@mail.ru>
2023-05-26 23:29:01 +0200

    to standart html

--- src/clas/InterfaceIPCalculatorCIDR.php
+++ src/clas/InterfaceIPCalculatorCIDR.php
@@ -26,49 +26,78 @@ class InterfaceIPCalculatorCIDR
         if ($this->interfaceTrueFalse->showUserMenu()) ;
         
         else if ($_SESSION['ipSSIDR']==0) {
-
             new Mask4BytTo32Bit;
+            echo "<p>
+                    Вы проверяете адрес: $this->ip
+                  </p>";
+
+            echo "<p>
+                    Двоичная маска подсети: 
+                    {$this->maska->maska2()}
+                  </p>";
+
+            echo "<p>
+                   Десятичная маска подсети: 
+                   {$this->maska->maska10($this->maska->maska2())}
+                  </p>";
+
+            $maska2 = $this->network->networkAddressByMaskAndIp(
+                      $this->ip10To2->ip10To2($this->ip),
+                      $this->maska->maska2());
+            echo "<p>
+                    Адрес сети: 
+                    $maska2
+                  </p>";
+
+            $maska2 = $this->first->firstAddress(
+                          $this->network->networkAddressByMaskAndIp(
+                              $this->ip10To2->ip10To2($this->ip),
+                              $this->maska->maska2()
+                          )
+                       );
+            echo "<p>
+                    Первый адрес в сети: 
+                    $maska2
+                  </p>";
+
+            $numerHost = NumerHost::createNumerHost()->numerHost();
+            echo "<p>
+                    Число хостов в сети: 
+                    $numerHost
+                  </p>";
+
+            echo '<p>Последний адрес в сети: '
+                   .$this->first->firstAddress(
+                        $this->network->networkAddressByMaskAndIp(
+                            $this->ip10To2->ip10To2($this->ip),
+                            $this->maska->maska2()
+                        ),
+                        NumerHost::createNumerHost()->numerHost()
+                    ).
+                  '</p>';
+
+            echo '<p>
+                    Широковещательный адрес в сети: '.
+                    $this->first->firstAddress(
+                        $this->network->networkAddressByMaskAndIp(
+                            $this->ip10To2->ip10To2($this->ip),
+                            $this->maska->maska2()
+                        ),
+                        (
+                            1+NumerHost::createNumerHost()->numerHost()
+                        )
+                    ).'</p>';
             
-            echo '<p>Вы проверяете адрес: '.$this->ip.'</p>';
-
-            echo '<p>Двоичная маска подсети: '.$this->maska->maska2().'</p>';
-
-            echo '<p>Десятичная маска подсети: '.$this->maska->maska10(
-                                                     $this->maska->maska2()
-                                                     ).'</p>';
-
-            echo '<p>Адрес сети: '.$this->network->networkAddressByMaskAndIp(
-                                       $this->ip10To2->ip10To2($this->ip),
-                                       $this->maska->maska2()
-                                       ).'</p>';
-
-            echo '<p>Первый адрес в сети: '.$this->first->firstAddress(
-                                                $this->network->networkAddressByMaskAndIp(
-                                                    $this->ip10To2->ip10To2($this->ip),
-                                                    $this->maska->maska2()
-                                                    )
-                                            ).'</p>';
-
-            echo '<p>Число хостов в сети: '.NumerHost::createNumerHost()->numerHost().'<p>';
-
-            echo '<p>Последний адрес в сети: '.$this->first->firstAddress(
-                                                   $this->network->networkAddressByMaskAndIp(
-                                                        $this->ip10To2->ip10To2($this->ip),
-                                                        $this->maska->maska2()
-                                                    ),
-                                                NumerHost::createNumerHost()->numerHost()
-                                                ).'</p>';
-
-            echo '<p>Широковещательный адрес в сети: '.$this->first->firstAddress(
-                                                           $this->network->networkAddressByMaskAndIp(
-                                                               $this->ip10To2->ip10To2($this->ip),
-                                                               $this->maska->maska2()),
-                                                               (1+NumerHost::createNumerHost()->numerHost()
-                                                               )
-                                                        ).'</p>';
-            
-            echo '<form action="IPCalculator.php" method="post">
-                      <input type="submit" name="ipSSIDRreset" value="Вернуться" class="button-ipS btn">
+            echo '<form 
+                    action="IPCalculator.php" 
+                    method="post"
+                  >
+                    <input 
+                      type="submit" 
+                      name="ipSSIDRreset" 
+                      value="Вернуться" 
+                      class="button-ipS btn"
+                    >
                   </form>';
         }
     }
